3b539b00c9b4feee0c71597ba52701e2af898914
Paula Cordero Encinar <pc2222@ic.ac.uk>
2025-12-17 18:33:44 +0000

    lil update

--- examples/NLG/create_datasets.sh
+++ examples/NLG/create_datasets.sh
@@ -3,15 +3,15 @@
 echo "creating e2e datasets..."
 path=data/e2e
 echo "train..."
-python src/format_converting_e2e.py $path/train.txt $path/train_formatted.jsonl
-python src/gpt2_encode.py --vocab vocab --input $path/train_formatted.jsonl --output $path/train.jsonl --add_bos --add_eos
+python3 src/format_converting_e2e.py $path/train.txt $path/train_formatted.jsonl
+python3 src/gpt2_encode.py --vocab vocab --input $path/train_formatted.jsonl --output $path/train.jsonl --add_bos --add_eos
 echo "test..."
-python src/format_converting_e2e.py $path/test.txt $path/test_formatted.jsonl
-python src/gpt2_encode.py --vocab vocab --input $path/test_formatted.jsonl --output $path/test.jsonl --add_bos --add_eos
+python3 src/format_converting_e2e.py $path/test.txt $path/test_formatted.jsonl
+python3 src/gpt2_encode.py --vocab vocab --input $path/test_formatted.jsonl --output $path/test.jsonl --add_bos --add_eos
 
 echo "valid..."
-python src/format_converting_e2e.py $path/valid.txt $path/valid_formatted.jsonl
-python src/gpt2_encode.py --vocab vocab --input $path/valid_formatted.jsonl --output $path/valid.jsonl --add_bos --add_eos
+python3 src/format_converting_e2e.py $path/valid.txt $path/valid_formatted.jsonl
+python3 src/gpt2_encode.py --vocab vocab --input $path/valid_formatted.jsonl --output $path/valid.jsonl --add_bos --add_eos
 
 #echo "creating webnlg datasets..."
 #path=data/webnlg_challenge_2017
